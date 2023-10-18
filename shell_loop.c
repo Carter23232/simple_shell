99b7a44a1ffacd73c311446e6bfb3a754f8a020d
Enock <asanteenoch10@gmail.com>
2023-10-18 22:50:36 +0200

    fixed

--- shell_loop.c
+++ shell_loop.c
@@ -129,7 +129,6 @@ void fork_cmd(info_t *info)
 	child_pid = fork();
 	if (child_pid == -1)
 	{
-		/* TODO: PUT ERROR FUNCTION */
 		perror("Error:");
 		return;
 	}
@@ -142,7 +141,7 @@ void fork_cmd(info_t *info)
 				exit(126);
 			exit(1);
 		}
-		/* TODO: PUT ERROR FUNCTION */
+
 	}
 	else
 	{
